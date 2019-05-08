964460f3e43bd1c2c7afab9214ca39f9cc7efb15
ngrande <devlup@outlook.com>
2019-05-08 16:23:31 +0200

    Only run script on vwd-archer

--- xrandr_vwd.sh
+++ xrandr_vwd.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
-xrandr --output DP-2 --auto --right-of DVI-I-1
-xrandr --output DP-2 --auto --left-of DVI-I-1
-xrandr --output DB-2 --rotate right
+exp="vwd-archer"
+if [ "$(hostname)" == "$exp" ]; then
+	xrandr --output DVI-I-1 --primary
+	xrandr --output DP-2 --auto --right-of DVI-I-1
+	xrandr --output DP-3 --auto --left-of DVI-I-1
+	xrandr --output DB-2 --rotate right
+else
+	echo "This script was made especially for '$exp'! Will not run here on: $(hostname)"
+fi
