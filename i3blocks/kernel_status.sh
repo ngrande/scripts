5065e28e5b8cf50d443a1e3ba552966466365285
ngrande <devlup@outlook.com>
2019-05-13 23:09:15 +0200

    Show active kernel

--- i3blocks/kernel_status.sh
+++ i3blocks/kernel_status.sh
@@ -3,12 +3,12 @@
 # script for i3blocks assumes you have arch linux running
 # hope you have rofi installed...
 
-if [ "$BLOCK_BUTTON" == "1" ]; then
+if [ $BLOCK_BUTTON -eq 1 ]; then
 
 	repo=$(pacman -Si linux | grep -i version | awk '{print $3}')
 	inst=$(pacman -Q linux | awk '{print $2}')
 	curr=$(uname -r)
-	echo -e "Repo: $repo\nInst: $inst\nCurr: $curr" | rofi -dmenu -lines 3 -hide-scrollbar -p "Kernel" -width -30 &> /dev/null
+	echo -e "Repo: $repo\nInst: $inst\nCurr: $curr" | rofi -dmenu -lines 3 -hide-scrollbar -p "Kernel" -width -30 -a 2 &> /dev/null
 	# we are done, return from here
 	return
 
