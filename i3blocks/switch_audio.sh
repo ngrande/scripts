d5f0045fbee5267c9172585fbf0d467cefebbb38
ngrande <devlup@outlook.com>
2019-05-14 20:45:16 +0200

    Make audio script smarter

---
+++ i3blocks/switch_audio.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+mode=""
+case "$1" in 
+	"sink")
+		mode="sink"
+		;;
+	"source")
+		mode="source"
+		;;
+	*)
+		echo "Invalid! Allowed: 'sink' or 'source'"
+		exit 1
+		;;
+esac
+
+active="0"
+pacmd list-${mode}s | grep -E "linked by: [1-9][0-9]?"
+
+filter_name() {
+	echo $( echo $1 | awk -F "alsa_output." '{print $2}' | awk -F ">" '{print $1}')
+}
+
+if [ $? ];
+then
+	# when more than one is actively linked, i will just show the first?
+	linked=$(pacmd list-${mode}s | grep -E "linked by: [1-9][0-9]?" -B 19 | head -n 1)
+
+	active=$(filter_name $(echo $linked | awk '{print $2}'))
+fi
+
+avail=$(pacmd list-${mode}s | grep "name:" | grep "output" | awk -F "name:" '{print $2}')
+list=""
+active_ind=""
+i=0
+for sink in $avail;
+do
+	sink=$(filter_name $sink)
+	if [ "$active" == "$sink" ];
+	then
+		active_ind=$i
+	fi
+	i=$(($i+1))
+	list="$list$sink\n"
+done
+selected=$( printf "$list" | rofi -dmenu -width -50 -lines 10 -p "Choose $mode" -a $active_ind)
+
+# magic 20?
+cmd="pacmd set-default-${mode} alsa_output.${selected}"
+eval $cmd
--- i3blocks/switch_audio_out.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-active=$(pacmd list-sinks | grep "RUNNING" -B3 | grep "name:" | awk '{print $2}')
-
-selected=$(pacmd list-sinks | grep "name:" | grep "output" | awk -F "alsa_output." '{print $2}' | awk -F ">" '{print $1}' | rofi -dmenu -width -50 -lines 10 -p "DEFAULT: $active")
-
-# magic 20?
-cmd="pacmd set-default-sink alsa_output.${selected}"
-eval $cmd
