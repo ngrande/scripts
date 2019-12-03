f6c37b91b838296855a5e86031b757edf59d5543
ngrande <devlup@outlook.com>
2019-12-03 12:46:40 +0100

    Fix kernel status script

--- i3blocks/kernel_status.sh
+++ i3blocks/kernel_status.sh
@@ -25,11 +25,13 @@ if [ ${#uname_r} -gt 4 ]; then
 fi
 
 # get the rel part (like "arch1-1")
-kernel_rel=$(uname -r | awk -F - '{print $2"-"$3}')
+#kernel_rel=$(uname -r | awk -F - '{print $2"-"$3}')
+kernel_rel=$(uname -r | awk -F "-arch" '{print $1"."$2}')
 # combine both to compare to package name (like "5.0.arch1-1")
 installed=$(pacman -Q linux | awk '{print $2}')
 
-active=("$kernel_vers.$kernel_rel")
+#active=("$kernel_vers.$kernel_rel")
+active=("$kernel_rel")
 
 # first check if new kernel available for download
 repo=$(pacman -Si linux | grep Version | awk '{print $3}')
