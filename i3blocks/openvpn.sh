5dcce2d67f4859d66cd1755c8371d5d131b1e0f7
ngrande <devlup@outlook.com>
2020-04-23 16:19:38 +0200

    Add openvpn controls

---
+++ i3blocks/openvpn.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+USER=$(cat ~/.openvpn_script | grep username | awk -F "=" '{print $2}')
+SECRET=$(cat ~/.openvpn_script | grep secret | awk -F "=" '{print $2}')
+FIX_PW=$(cat ~/.openvpn_script | grep password | awk -F "=" '{print $2}')
+VPN_CON_NAME=$(cat ~/.openvpn_script | grep connection | awk -F "=" '{print $2}')
+CONN_ARG=$1
+
+if [ "$BLOCK_BUTTON" == "1" ];
+then
+	CONN_ARG="connect"
+elif [ "$BLOCK_BUTTON" == "2" ];
+then
+	CONN_ARG="disconnect"
+fi
+
+openvpn() {
+	if [ "$CONN_ARG" == "connect" ]; then
+		AUTH_CODE=`oathtool --totp=sha1 -b $SECRET`
+		PW=$FIX_PW$AUTH_CODE
+		/usr/bin/nmcli c modify id $VPN_CON_NAME vpn.user-name $USER
+		echo $PW | /usr/bin/nmcli c up $VPN_CON_NAME --ask
+		sudo ln -sf /etc/resolv.conf.vwd /etc/resolv.conf
+		# this route breaks my internet connection but is added with the .ovpn
+		sudo ip route del default via 172.25.29.129 dev tun0 proto static metric 50
+		echo "connected."
+	elif [ "$CONN_ARG" == "disconnect" ]; then
+		/usr/bin/nmcli c down $VPN_CON_NAME
+		sudo ln -sf /etc/resolv.conf.private /etc/resolv.conf
+		echo "disconnected."
+	fi
+}
+
+openvpn
+
+if [ "$(/usr/bin/nmcli c show --active | grep $VPN_CON_NAME)" ]; then
+	echo "active"
+else
+	echo "inactive"
+fi
