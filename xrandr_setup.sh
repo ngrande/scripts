2b06fd5abb82239cf5a8665128183e74d1b47356
ngrande <devlup@outlook.com>
2020-12-14 15:17:42 +0100

    Update xrandr script

--- xrandr_setup.sh
+++ xrandr_setup.sh
@@ -2,10 +2,12 @@
 
 if [ $(xrandr --query | grep ' connected' | wc -l) -gt 3 ];
 then
+	# left is 1-2
+	# right is 1-1
 	xrandr --output eDP-1 --off
-	xrandr --output DP-1-2 --auto --primary
-	xrandr --output DP-1-3 --auto --right-of DP-1-2 --rotate right
-	xrandr --output DP-1-1 --auto --left-of DP-1-2 
+	xrandr --output DP-1-3 --auto --primary --rotate normal
+	xrandr --output DP-1-1 --auto --right-of DP-1-3 --rotate right
+	xrandr --output DP-1-2 --auto --left-of DP-1-3 --rotate normal
 else
 	dis=`xrandr | grep  "disconnected" | awk '{print $1}'`
 	for d in $dis; do
